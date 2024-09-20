c624920ec658da59289b22cdd5a83182b3a4bfba
Mark Nellemann <mark.nellemann@gmail.com>
2024-09-20 13:37:28 +0200

    Update scripts and java options.

--- doc/run.sh
+++ doc/run.sh
@@ -9,8 +9,8 @@ export DISPLAY=:0
 # JavaFX
 export PATH_TO_FX=/usr/share/openjfx/lib
 
-# Java options
-JAVA_OPTS="-XX:+ExitOnOutOfMemoryError"
+# Java options for Java 21 (ZGC with
+JAVA_OPTS="-XX:+ExitOnOutOfMemoryError -XX:+UseZGC -XX:+ZGenerational -XX:ConcGCThreads=1"
 
 exec 2>&1
 exec java $JAVA_OPTS -jar "${BASE_DIR}/demo-all.jar"
